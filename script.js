d0a96803ef99860f606da185eeaf6e7183361377
nishan alderson <40016417+nishanalderson18@users.noreply.github.com>
2019-01-19 04:55:20 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const colorBtn = document.querySelector('.colorBtn');
+const bodyBcg = document.querySelector('body');
+
+const colors = ["437742","#15658D","#D67FFC","#3b5968"];
+
+colorBtn.addEventListener('click',changeColor);
+function changeColor() {
+    //bodyBcg.style.backgroundColor = "blue";
+    let random = Math.floor(Math.random()*colors.length)
+    bodyBcg.style.backgroundColor = colors[random];
+}
+
